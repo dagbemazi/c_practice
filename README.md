15c38ed4a3cc6a8e386b8496a2fd5f1cf9e78b5f
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-11-15 17:17:06 +0000

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # c_practice
 Practice projects completed in C
+
+All your files will be compiled using *gcc*, using the options:
+ *-Wall -Werror -Wextra -pedantic -std=gnu89*
+
+ The code uses the **Betty** style guide for C.
---
+++ chp2/parkinson.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - print parkinsons law
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	printf("Parkinson's Law: \nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+
+	return (0);
+}
